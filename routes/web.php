b5edddf78dcc302ca167a8d98e2f9400369a0cd1
pierfrancescoscar <scarinci401@gmail.com>
2022-01-31 15:11:03 +0100

    ADD: dashboard content-setup home controller w/any

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,24 @@ use Illuminate\Support\Facades\Route;
 
 // Home
 Route::get('/', function () {
-    return view('welcome');
+    return view('guests.home');
 });
 // Routes authenticate
 Auth::routes();
 
 // Admin routes
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function() {
+        Route::get('/home', 'HomeController@index')->name('home');
+    });
+
+// Home front
+
+Route::get('{any?}', function () {
+    return view('guests.home');
+})-where('any', '.' );
