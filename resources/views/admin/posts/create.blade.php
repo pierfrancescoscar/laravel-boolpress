b579c0a178c67c20137ca266e10641c8b54d49aa
pierfrancescoscar <scarinci401@gmail.com>
2022-02-01 14:41:13 +0100

    ADD: complete form of create page

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -7,10 +7,20 @@
         <form action="{{ route('admin.posts.store') }}" method="POST">
             @csrf
 
+            {{-- Title --}}
             <div class="mb-5">
-                <label class="form-label" for="title">Title</label>
+                <label class="form-label" for="title">Title*</label>
                 <input class="form-control" type="text" name="title" id="title">
             </div>
+            {{-- Content --}}
+            <div class="mb-5">
+                <label class="form-label" for="content">Content*</label>
+                <textarea class="form-control" type="text" name="content" id="content" rows="6"></textarea>
+            </div>
+            {{-- Submit --}}
+            
+            <button class="btn btn-primary" type="submit">Create Post</button>
+
         </form>
     </div>
 @endsection
