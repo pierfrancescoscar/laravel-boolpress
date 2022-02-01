4de329b6800d70e32e80b4b2be7877bca2d5465b
pierfrancescoscar <scarinci401@gmail.com>
2022-02-01 14:34:15 +0100

    FIX: correct slug, space into title tag

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -15,7 +15,7 @@ class PostsTableSeeder extends Seeder
         for($i = 0; $i < 3; $i++) {
             $new_post = new Post();
 
-            $new_post->title = 'Post title' . ($i + 1);
+            $new_post->title = 'Post title ' . ($i + 1);
             $new_post->slug = Str::slug($new_post->title, '-');
             $new_post->content = 'Lorem ipsum dolor';
 
