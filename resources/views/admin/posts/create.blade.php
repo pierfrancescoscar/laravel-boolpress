452364ce1e565301d8d8b7dd390174daf345496c
pierfrancescoscar <scarinci401@gmail.com>
2022-02-03 16:09:29 +0100

    ADD: to create a new post section select btn with categories

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -35,6 +35,21 @@
                 <div class="text-danger">{{ $message }}</div>
                 @enderror
             </div>
+
+            {{-- Category --}}
+            <div class="mb-5">
+                <label for="category_id">Category</label>
+                <select class="form-control" name="category_id" id="category_id">
+                    <option value="">Uncategorized</option>
+                    @foreach ($categories as $category)
+                    <option value="{{ $category->id }}"  @if ($category->id == old('category_id')) selected @endif>
+                        {{ $category->name }}
+                    </option>
+                    @endforeach
+                </select>
+                    
+            </div>
+
             {{-- Submit --}}
             <button class="btn btn-primary" type="submit">Create Post</button>
 
