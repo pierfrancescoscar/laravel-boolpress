d2c4fb8cc02fddbda7884419583e25a636ffeb79
pierfrancescoscar <scarinci401@gmail.com>
2022-02-03 16:23:40 +0100

    ADD: relative posts categories when show is clicked

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -5,15 +5,23 @@
         <h1 class="mb-5">{{ $post->title }}</h1>
 
         <div class="mb-5">
+            {{-- Relative Category --}}
+            <span class="mb-3">
+                <h5>Category:
+                {{ $post->category->name }}
+                </h5>
+            </span>
+            {{-- Action buttons - EDIT, BACK TO ARCHIVE --}}
             <a class="btn btn-warning" href="{{ route('admin.posts.edit', $post->id) }}">Edit</a>
             <a class="btn btn-danger" href="{{ route('admin.posts.index', $post->id) }}">Back to archive</a>
-        </div>
 
+        </div>
+        {{-- Content --}}
         <div class="row">
             <div class="col-6">
                 {!! $post->content !!}
             </div>
-
+            {{-- Image space --}}
             <div class="col-6">
                 Here goes image
             </div>
