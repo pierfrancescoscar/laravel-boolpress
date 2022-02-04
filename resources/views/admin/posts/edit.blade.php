a07b85e5a979b9199009618f21a2aa78e0689963
pierfrancescoscar <scarinci401@gmail.com>
2022-02-04 15:52:44 +0100

    ADD: tags checkbox w/errors into edit blade

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -57,6 +57,27 @@
                     @enderror
             </div>
 
+            {{-- Tags --}}
+
+            <div class="mb-3">
+                <h4>Tags</h4>
+
+                @foreach ($tags as $tag)
+                <span class="d-inline-block mr-3">
+                    <input type="checkbox" name="tags[]" id="tag{{ $loop->iteration }}" value="{{ $tag->id }}"
+                    @if(in_array($tag->id, old('tags', []))) checked @endif>
+
+                    <label for="tag{{ $loop->iteration }}">
+                        {{ $tag->name }}
+                    </label>
+                </span>
+                @endforeach
+                {{-- Tag Error --}}
+                @error('tags')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+
             {{-- Submit --}}
             <button class="btn btn-primary" type="submit">Update Post</button>
 
